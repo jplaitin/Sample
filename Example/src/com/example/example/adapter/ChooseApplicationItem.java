396e01c4db617fd3bacf01e8d5b071e0b43e8ef3
jlaitinen <jarno.laitinen@eficode.com>
2015-03-06 12:18:59 +0200

    cleared

--- Example/src/com/example/example/adapter/ChooseApplicationItem.java
+++
@@ -1,8 +0,0 @@
-package com.example.example.adapter;
-
-import android.graphics.drawable.Drawable;
-
-public class ChooseApplicationItem {
-	public String packageName, className, appName;
-	public Drawable icon;
-}
--- Example/src/com/example/example/adapter/ChooseApplicationSelectionListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.example.adapter;
-
-public interface ChooseApplicationSelectionListener {
-	public void itemSelected(ChooseApplicationItem selectedItem);
-}
